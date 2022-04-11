e9534ad119fdd6075a02c11179e881d6e82bc5c7
Kapelu <ddaniel_calderon@hotmail.com>
2022-04-11 13:58:19 -0300

    11/04/22

--- Modulo1-Foundations/00-IntroToCS/homework/homework.js
+++ Modulo1-Foundations/00-IntroToCS/homework/homework.js
@@ -1,16 +1,13 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  let sum = 0
-  for (let i = 0; i < num.length; i++) {
-    sum += +num[i] * 2 ** (num.length - 1 - i)
-  }
-  return sum
+  // tu codigo aca
+
 }
 
 function DecimalABinario(num) {
-  let number = parseInt(num),resultado = number.toString(2)
-  return resultado
+  // tu codigo aca
+
 }
 
 
