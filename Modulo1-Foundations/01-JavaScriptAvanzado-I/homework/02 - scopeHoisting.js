a8a89b37f1a17dc8dc6c7a8f1daa4109a7e5e820
Kapelu <ddaniel_calderon@hotmail.com>
2022-04-12 23:05:18 -0300

    Completado 12/04/22

--- Modulo1-Foundations/01-JavaScriptAvanzado-I/homework/02 - scopeHoisting.js
+++
@@ -1,7 +0,0 @@
-//$ Investiga cuál es la diferencia entre declarar una variable con `var` y directamente asignarle un valor.
-
-var instructor = 'Tony'
-if (true) {
-  var instructor = 'Franco'
-}
-console.log(instructor)
